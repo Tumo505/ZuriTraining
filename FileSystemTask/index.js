32b796d2d0251cc1ff0bc6da97a32b67944bdc04
Tumo505 <kgosiyametumo44@gmail.com>
2021-04-19 21:34:13 +0200

    fetch json

---
+++ FileSystemTask/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const fetch = require("node-fetch")
+
+
+
+
+async function getPosts() {
+    const posts = await fetch("http://jsonplaceholder.typicode.com/posts");
+    let response = await posts.json()
+        // console.log(response)
+
+    fs.readFile('./result/posts.json', 'utf8', (err, data) => {
+        if (err) throw err
+
+        // posts = JSON.parse(content);
+        // data.push(posts)
+
+        fs.writeFile('./result/posts.json', JSON.stringify(posts), (err) => {
+            if (err) throw err
+        })
+    })
+
+}
+getPosts()

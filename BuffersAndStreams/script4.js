eb7722f7889367502ac5ae6afe7cc508f8a2e4db
Tumo505 <kgosiyametumo44@gmail.com>
2021-05-09 23:15:17 +0200

    updates

--- BuffersAndStreams/script4.js
+++ BuffersAndStreams/script4.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 
 
 // Create ReadStream
-const dataReadStream = fs.createReadStream(`${__dirname}/data.json`, 'utf8');
+// const dataReadStream = fs.createReadStream(`${__dirname}/data.json`, 'utf8');
 
 // Write Stream
-const dataWriteStream = fs.createWriteStream(`${__dirname}/write.txt`,);
+// const dataWriteStream = fs.createWriteStream(`${__dirname}/write.txt`,);
 
 // // Read Stream
 // dataReadStream.on('data', (chunk) => {
@@ -20,5 +20,18 @@ const dataWriteStream = fs.createWriteStream(`${__dirname}/write.txt`,);
 // })
 
 // PIPE
-dataReadStream.pipe(dataWriteStream)
+// dataReadStream.pipe(dataWriteStream)
+
+// SEND PIPE STREAM TO CLIENT
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    const dataReadStream = fs.createReadStream(`${__dirname}/data.json`, 'utf8');
+    dataReadStream.pipe(res);
+});
+
+// listen
+server.listen(5000, 'localhost', () => {
+    console.log("Now listening on port 5000")
+});
 

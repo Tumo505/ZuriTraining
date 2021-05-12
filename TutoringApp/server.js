ff75ee714750305da8d4947268e35174ce9de2f2
Tumo505 <kgosiyametumo44@gmail.com>
2021-05-12 09:48:14 +0200

    updates

--- TutoringApp/server.js
+++ TutoringApp/server.js
@@ -8,7 +8,9 @@
 */ 
 
 const express = require('express')
-const connectDB = require('./db');
+const connectDB = require('./db')
+require('dotenv').config();
+const { PORT } = process.env;
 
 
 // Connect to db
@@ -22,3 +24,9 @@ app.use(express.json({ extended: false }));
 
 // Create a basic express route
 app.get('/', (req, res) => res.json({ message: "Welcome to Tutoring App!" }))
+
+// PORT
+constport = process.env.PORT || PORT;
+
+// Listen to connection
+app.listen(port, () => console.log(`message running on port ${port}`));
